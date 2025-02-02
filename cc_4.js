fd4edb26f3d53f6927eb21469c0692fabeab172c
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-02-02 12:18:40 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,10 @@
+// Task 1 If Statements
+
+let purchaseAmount = 350; // Set a purchase amount
+let finalAmount = purchaseAmount;
+
+if(purchaseAmount > 100) {
+    finalAmount = purchaseAmount * 0.9; 
+} // Setting a 10% discount if purchase amount is over 100
+console.log(`Final amount after discount: $${finalAmount}`); // Logging purchase amount after discount in console
+
