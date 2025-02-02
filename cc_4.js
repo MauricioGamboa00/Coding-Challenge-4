2b8924158dd4aa4e2ce0d82e8af2e2a2ea7dc868
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-02-02 12:28:01 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -8,3 +8,14 @@ if(purchaseAmount > 100) {
 } // Setting a 10% discount if purchase amount is over 100
 console.log(`Final amount after discount: $${finalAmount}`); // Logging purchase amount after discount in console
 
+// Task 2 For Loop
+
+let sales = [120, 85, 200, 150, 90]; // Created an array for sales figures 
+let totalSales = 0;
+
+for (let i = 0; i < sales.length; i++) {
+    totalSales += sales[i]; // Adding up the sales to calculate Total Sales
+}
+
+console.log(`Total Sales: $${totalSales}`); // Logging Total Sales amount to console
+
